d55e9f422ef7bdea3822f20d6acf1040cb80efbe
skvs32 <sigge123123@gmail.com>
2018-02-19 20:24:03 +0100

    Lösenord

---
+++ eyyo.py
@@ -0,0 +1,9 @@
+import getpass
+while (True): 
+    Kod = getpass.getpass ("Lösenord ")
+    if (Kod=="hej"):
+        print ("hej " +"Sigge" + "!")
+        break   
+    else:
+        print ("försök igen")
+        
